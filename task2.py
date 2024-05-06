9b0aa50cb02f2602b853739a02c8d1b1472cb262
nahidallhv <161585836+nahidallhv@users.noreply.github.com>
2024-05-06 14:02:50 +0400

    py

---
+++ task2.py
@@ -0,0 +1,59 @@
+#print("hello world!")
+
+
+
+#1
+# def square_num(list):
+#     squares = [i for i in lst if i >= 0 and (i ** 0.5).is_integer()]
+#     return squares
+# mylist = [-4, -16, 0, 1, 4, 5, 9, 16, 25, 36, 49, 64, 81, 100]
+# print("kvadrati olan ededler : ", square_num(mylist))
+
+#2
+# def tekrar_num(lst):
+#     tekrar = list(set(lst))
+#     return tekrar
+# input_list = [-1, 1, 2, 2, 6, 7, 7, 'say']
+# result = tekrar_num(input_list)
+# print("Tekrarlanmayan elementlər:", result)
+
+#3
+# def vurm_elements(lst):
+#     result = 1  
+#     for num in lst:  
+#         result *= num  
+#     return result
+# input_list = [5, 6, 7]
+# result = vurm_elements(input_list)
+# print("Vurma : ", result)
+
+#4
+# def find_nums(n):
+#     bolenler = [i for i in range(1, n+1) if n % i == 0]
+#     return bolenler
+# number = 12
+# result = find_nums(number)
+# print(f"{number} reqeminin bölenleri:", result)
+
+#5
+# mylist = ['may', 'iyun', 'iyul']
+# ay_lengths = {ay: len(ay) for ay in mylist}
+# print(ay_lengths)
+
+#6
+
+# names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+# lowercase_names = [name.split()[0].lower() for name in names]
+# print(lowercase_names)
+
+##7
+
+# nums1 = [1, 2, 3]
+# nums2 = [4, 5, 6]
+# results = [(x + y) / 2 for x, y in zip(nums1, nums2)]
+# print("Ortalama : ", results)
+
+
+
+
+
